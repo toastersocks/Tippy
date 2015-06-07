a54e7fb3ff96f6633f4a6c1608399ce597d111e1
James Pamplona <pamplonajames@gmail.com>
2015-06-07 09:55:48 -0700

    Better KVO compliance

--- Tippy/TipoutModel.swift
+++ Tippy/TipoutModel.swift
@@ -22,9 +22,13 @@ class TipoutModel: NSObject {
         return round((total * 0.3), toNearest: 0.25)
     }
     
-    dynamic var workersHours = [Double]()
+    dynamic var workersHours = [Double]() {
+        didSet {
+            debugPrintln(workersHours)
+        }
+    }
     
-    var workersTipOuts: [Double] {
+    dynamic var workersTipOuts: [Double] {
         let tipouts = workersHours.map {
         return round(((self.total - self.kitchenTipout) / self.totalWorkersHours * $0), toNearest: 0.25)
         }
@@ -36,17 +40,17 @@ class TipoutModel: NSObject {
         return workersHours.reduce(0, combine: {$0 + $1})
     }
     
+    // MARK: - KVO
     class func keyPathsForValuesAffectingKitchenTipout() -> Set<NSObject> {
     return Set(["total"])
     }
     
-    override dynamic class func keyPathsForValuesAffectingValueForKey(key: String) -> Set<NSObject> {
-        var keypaths = super.keyPathsForValuesAffectingValueForKey(key)
-        
-        if key == "kitchenTipout" {
-            keypaths.insert("total")
-        }
-        return keypaths
+    class func keyPathsForValuesAffectingTotalWorkersHours() -> Set<NSObject> {
+        return Set(["workersHours"])
+    }
+    
+    class func keyPathsForValuesAffectingWorkersTipOuts() -> Set<NSObject> {
+        return Set(["workersHours", "total"])
     }
     
 }
