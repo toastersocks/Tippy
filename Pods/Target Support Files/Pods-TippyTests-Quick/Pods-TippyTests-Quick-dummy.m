cc0208485d44c8d1f4abe43dd852f6a254fe955d
James Pamplona <pamplonajames@gmail.com>
2015-06-11 13:15:05 -0700

    Added testing frameworks

---
+++ Pods/Target Support Files/Pods-TippyTests-Quick/Pods-TippyTests-Quick-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TippyTests_Quick : NSObject
+@end
+@implementation PodsDummy_Pods_TippyTests_Quick
+@end
---
+++ TippyTests/TipoutModelSpec.swift
@@ -0,0 +1,9 @@
+//
+//  ModelSpec.swift
+//  Tippy
+//
+//  Created by James Pamplona on 6/11/15.
+//  Copyright (c) 2015 James Pamplona. All rights reserved.
+//
+
+import Foundation
