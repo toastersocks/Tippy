acc50fc37afe8fc9d6849ec0d4723992734bf7c1
James Pamplona <pamplonajames@gmail.com>
2015-06-11 13:26:47 -0700

    Added test for tipout totals

--- TippyTests/TipoutModelSpec.swift
+++ TippyTests/TipoutModelSpec.swift
@@ -1,5 +1,5 @@
 //
-//  ModelSpec.swift
+//  TipoutModelSpec.swift
 //  Tippy
 //
 //  Created by James Pamplona on 6/11/15.
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+//import ReactiveCocoa
+import Tippy
+import Fox
+import Nimble
+import NimbleFox
+import Quick
+
+
+private func generateDouble(f: Double -> Bool) -> FOXGenerator {
+    return forAll(FOXDouble()) {
+        (floatNum: AnyObject!) -> Bool in
+        return f(floatNum as! Double)
+    }
+}
+
+class TipoutModelSpec: QuickSpec {
+    override func spec() {
+
+        describe("a TipoutModel") {
+            var tipoutModel: TipoutModel!
+            
+            beforeEach {
+                tipoutModel = TipoutModel()
+//                tipoutModel.total = 100.9
+                tipoutModel.workersHours = [4.0, 3.0, 1.0]
+            }
+            
+            describe("its total") {
+                it("should be equal to the total of all worker tipouts") {
+                    let property = generateDouble() {
+                        (num: Double) in
+                        tipoutModel.total = num
+                        
+                        return tipoutModel.workersTipOuts.reduce(tipoutModel.kitchenTipout, combine:{ $0 + $1 }) == tipoutModel.total
+                    }
+//                    expect(tipoutModel.total).to(equal(tipoutModel.workersTipOuts.reduce(tipoutModel.kitchenTipout, combine:{ $0 + $1 })))
+                    expect(property).to(hold())
+                }
+            }
+        }
+    }
+}
