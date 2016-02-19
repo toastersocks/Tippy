0d1b2098609543203ef980825683c5ebdcdb107d
James Pamplona <pamplonajames@gmail.com>
2016-02-19 15:08:14 -0800

    Code cleanup

--- Tippy/JCPTextField.swift
+++ Tippy/JCPTextField.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class JCPTextField: UITextField {
-
+    
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-        // Drawing code
+    // Drawing code
     }
     */
-
+    
     override func intrinsicContentSize() -> CGSize {
+        
         if let text = self.text where !text.isEmpty {
             let textSize = (text as NSString).sizeWithAttributes(typingAttributes)
             let width = textSize.width +
@@ -32,33 +33,4 @@ class JCPTextField: UITextField {
         }
     }
     
-    func tableViewCell() -> UIView? {
-        var aView: UIView? = superview
-        
-        while !(aView is UITableViewCell) && aView != nil {
-            aView = aView?.superview
-        }
-        /*while var aView: UIView? = superview where !(aView is UITableViewCell) {
-            aView = superview
-        }*/
-        
-//        guard let cell = aView as? TableViewCell else { fatalError("Wrong cell type. Expected a TableViewCell") }
-        return aView
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        tableViewCell()?.touchesBegan(touches, withEvent: event)
-    }
-    
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        tableViewCell()?.touchesMoved(touches, withEvent: event)
-    }
-    
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        tableViewCell()?.touchesEnded(touches, withEvent: event)
-    }
-    
-    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
-        tableViewCell()?.touchesCancelled(touches, withEvent: event)
-    }
 }
