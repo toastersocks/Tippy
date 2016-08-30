e41d553328ca7d570acf1465dd693d82adcb9fe4
James Pamplona <pamplonajames@gmail.com>
2016-08-30 09:45:29 -0700

    Fix insert/delete animation

--- Tippy/StackLayout.swift
+++ Tippy/StackLayout.swift
@@ -12,7 +12,13 @@ class StackLayout: UICollectionViewFlowLayout {
 
     override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
         guard let attributes = layoutAttributesForItemAtIndexPath(itemIndexPath) else { fatalError("Couldn't retrieve layout attributes for item at index path \(itemIndexPath)") }
+        guard let collectionView = collectionView else { fatalError("Can't access collection View") }
+        
+        let numberOfItems = collectionView.numberOfItemsInSection(0)
         attributes.size.width = 0
+        attributes.size.height = collectionView.bounds.height - 10
+        let width = collectionView.bounds.width / CGFloat(numberOfItems)
+        attributes.frame.origin.x = width * CGFloat(itemIndexPath.item)
         
         return attributes
     }
@@ -25,4 +31,15 @@ class StackLayout: UICollectionViewFlowLayout {
         return attributes
     }
     
+    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let collectionView = collectionView else { fatalError("Can't access collection View") }
+        let attributes = UICollectionViewLayoutAttributes()
+        let numberOfItems = collectionView.numberOfItemsInSection(0)
+        let width = collectionView.bounds.width / CGFloat(numberOfItems)
+        let xOrigin = width * CGFloat(indexPath.item)
+        attributes.frame = CGRect(origin: CGPoint(x: xOrigin, y: 0),
+                                  size: CGSize(width: width, height: collectionView.bounds.height - 10))
+        
+        return attributes
+    }
 }
