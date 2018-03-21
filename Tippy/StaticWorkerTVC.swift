510efd7ded959d96ee3c9ff501c2dfd2f831674b
James Pamplona <pamplonajames@gmail.com>
2018-03-21 15:05:04 -0700

    swift 3 conversion

--- Tippy/StaticWorkerTVC.swift
+++ Tippy/StaticWorkerTVC.swift
@@ -219,7 +219,10 @@ class StaticWorkerTVC: WorkerTableViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        dump(sender)
+        dump(type(of: sender))
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
